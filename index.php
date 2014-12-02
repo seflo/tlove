71d43d5f23e3948f5c07d173e3b841ebb32c0438
seflo <joelopy@gmail.com>
2014-12-02 01:05:41 -0800

    General class CSS and nav bar setup
    Basic Navigation bar css was setup and tested across all PC browsers
    and iPhone 4.

--- index.php
+++ index.php
@@ -1,1 +1,47 @@
-hi
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Temporal Love Home</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
+	<meta name="description" content="The Official Page of the Temporal Love">
+	<meta name="application-name" content="">
+	<meta name="keywords" content="temporal love, music">
+
+	<link rel="canonical" href="http://www.temporallove.com">
+
+	<link rel="stylesheet" type="text/css" href="css/test.css">
+</head>
+<body>
+	<div id="intro">
+	</div>
+	<div id="anim">
+	</div>
+	<div id="nav">
+		<div class="button toggle">
+      		<img src="img/pause.png" class="imgResponsive">
+		</div>
+		<div class="bar">
+		</div>
+	</div>
+	<div id="main">
+	</div>
+	<div id="bg">
+		<div class="top">
+			<div style="background-color: white"></div>
+			<div style="background-color: yellow"></div>
+			<div style="background-color: cyan"></div>
+			<div style="background-color: #FF00FF"></div>
+			<div style="background-color: red"></div>
+			<div style="background-color: blue"></div>
+		</div>
+		<div class="bottom">
+			<div style="background-color: black"></div>
+			<div style="background-color: blue"></div>
+			<div style="background-color: red"></div>
+			<div style="background-color: #00FF00"></div>
+			<div style="background-color: cyan"></div>
+			<div style="background-color: yellow"></div>
+		</div>
+	</div>
+</body>
+</html>
